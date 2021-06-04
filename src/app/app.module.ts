98f3b311680ddabab8b53834b07ac2c47f89ee1f
kaushalmeena <kaushal.meena@aasaanjobs.com>
2021-06-05 01:10:05 +0530

    fix: correct imports and image styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LockComponent } from './pages/lock/lock.component';
+import { UnlockComponent } from './pages/unlock/unlock.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LockComponent,
+    UnlockComponent,
+    FaqsComponent
   ],
   imports: [
     BrowserModule,
