90a69ff708d9938acf2bff5b888aaa581c73a322
kaushalmeena <kaushal.meena@aasaanjobs.com>
2021-06-17 18:00:03 +0530

    removed unused ngOnInit hook

--- src/app/pages/faqs/faqs.component.ts
+++ src/app/pages/faqs/faqs.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faqs',
   templateUrl: './faqs.component.html',
   styleUrls: ['./faqs.component.scss']
 })
-export class FaqsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class FaqsComponent { }
