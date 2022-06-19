e816f9fa090c844edd8929f7a3a1647c9482b4d0
kaushalmeena <kaushal.meena1996@gmail.com>
2022-06-19 14:58:04 +0530

    feat: improve cloaking abilities

--- src/app/shared/utils/index.ts
+++ src/app/shared/utils/index.ts
@@ -1,6 +1,10 @@
-import AES from 'crypto-js/aes';
-import Utf8 from 'crypto-js/enc-utf8';
-import { DELIMITER_CHARACTER } from '../constants';
+import CryptoJS from 'crypto-js';
+import {
+  BITS_PER_CHAR,
+  MESSAGE_BORDER,
+  MESSAGE_FORMAT,
+  MESSAGE_HEADER,
+} from '../constants';
 
 export const fetchDarkMode = (): boolean => {
   let mode = false;
@@ -35,12 +39,9 @@ export const getBase64ImageFromBlob = (blob: Blob): Promise<string> =>
     const reader = new FileReader();
 
     reader.readAsDataURL(blob);
-    reader.onload = function () {
-      resolve(reader.result as string);
-    };
-    reader.onerror = function () {
-      reject(reader.error);
-    };
+
+    reader.onload = (event) => resolve(event.target?.result as string);
+    reader.onerror = (event) => reject(event.target?.error);
   });
 
 export const getImageDataFromBase64Image = (
@@ -76,107 +77,160 @@ export const getBase64ImageFromImageData = (imageData: ImageData): string => {
 
   if (context) {
     context.putImageData(imageData, 0, 0);
-    result = canvas.toDataURL();
+    result = canvas.toDataURL('image/png', '0.95');
   }
 
   return result;
 };
 
-export const getEncodedBase64Image = (
+export const getFormattedMessage = (message: string): string => {
+  const length = message.length.toString();
+  return MESSAGE_FORMAT.replace('<L>', length).replace('<M>', message);
+};
+
+export const getEncodedBase64Image = async (
   base64Image: string,
   message: string,
   password: string
-): string => {
-  let result = '';
-
+): Promise<string> => {
   const imageData = getImageDataFromBase64Image(base64Image);
   if (!imageData) {
-    return 'Invalid image detected.';
+    throw new Error('Cannot generate image data from base64 image');
   }
 
-  const ciphertext = AES.encrypt(message, password).toString();
+  const ciphertext = CryptoJS.AES.encrypt(message, password).toString();
 
-  const encryptedMessage = ciphertext + DELIMITER_CHARACTER;
+  const encryptedMessage = getFormattedMessage(ciphertext);
+
+  if (encryptedMessage.length > imageData.data.length) {
+    throw new Error('Message length is too large to hide in image');
+  }
 
   let binaryMessage = '';
-  let counter = 0;
-  let completed = false;
 
-  for (let i = 0; i < encryptedMessage.length; i++) {
+  for (let i = 0; i < encryptedMessage.length; i += 1) {
     binaryMessage += encryptedMessage[i]
       .charCodeAt(0)
       .toString(2)
-      .padStart(8, '0');
+      .padStart(BITS_PER_CHAR, '0');
   }
 
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    for (let offset = 0; offset < 3; offset++) {
-      if (counter < binaryMessage.length) {
-        imageData.data[i + offset] &= 254;
-        imageData.data[i + offset] += parseInt(binaryMessage[counter]);
-        counter += 1;
-      } else {
-        completed = true;
-        break;
-      }
-    }
-    if (completed) {
-      break;
-    }
+  for (let i = 0; i < binaryMessage.length; i += 1) {
+    imageData.data[i] = (imageData.data[i] & 254) + Number(binaryMessage[i]);
   }
 
-  result = getBase64ImageFromImageData(imageData);
+  const result = getBase64ImageFromImageData(imageData);
+  if (!result) {
+    throw new Error('Cannot generate base64 image from image data');
+  }
 
   return result;
 };
 
-export const getDecodedMessage = (
-  base64Image: string,
-  password: string
+export const getDecodedTextFromImageData = (
+  range: [number, number],
+  imageData: ImageData
 ): string => {
   let result = '';
 
+  let tempBits = '';
+  let tempChar = '';
+
+  const [startIndex, endIndex] = range;
+
+  for (let i = startIndex; i < endIndex; i += 1) {
+    if (imageData.data[i] % 2 === 0) {
+      tempBits += '0';
+    } else {
+      tempBits += '1';
+    }
+    if (tempBits.length === BITS_PER_CHAR) {
+      tempChar = String.fromCharCode(parseInt(tempBits, 2));
+      tempBits = '';
+      result += tempChar;
+    }
+  }
+
+  return result;
+};
+
+export const getMessageHeaderFromImageData = (imageData: ImageData): string =>
+  getDecodedTextFromImageData(
+    [0, MESSAGE_HEADER.length * BITS_PER_CHAR],
+    imageData
+  );
+
+export const getCiphertextRangeFromImageData = (
+  imageData: ImageData
+): [number, number] | null => {
+  let result = '';
+
+  let borderCount = 0;
+
+  let tempBits = '';
+  let tempChar = '';
+
+  const startIndex = MESSAGE_HEADER.length * BITS_PER_CHAR;
+  const endIndex = imageData.data.length;
+
+  for (let i = startIndex; i < endIndex; i += 1) {
+    if (imageData.data[i] % 2 === 0) {
+      tempBits += '0';
+    } else {
+      tempBits += '1';
+    }
+    if (tempBits.length === BITS_PER_CHAR) {
+      tempChar = String.fromCharCode(parseInt(tempBits, 2));
+      tempBits = '';
+      if (tempChar === MESSAGE_BORDER) {
+        borderCount += 1;
+        if (borderCount === 1) {
+          continue;
+        }
+        if (borderCount === 2) {
+          const ciphertextLength = Number(result);
+          const ciphertextStartIndex = i + 1;
+          const ciphertextEndIndex =
+            ciphertextStartIndex + ciphertextLength * BITS_PER_CHAR;
+          return [ciphertextStartIndex, ciphertextEndIndex];
+        }
+        return null;
+      }
+      if (isNaN(Number(tempChar))) {
+        return null;
+      }
+      result += tempChar;
+    }
+  }
+
+  return null;
+};
+
+export const getDecodedMessage = async (
+  base64Image: string,
+  password: string
+): Promise<string> => {
   const imageData = getImageDataFromBase64Image(base64Image);
   if (!imageData) {
-    return 'Invalid image detected.';
+    throw new Error('Cannot generate image data from base64 image');
   }
 
-  let ciphertext = '';
-  let tempBits = '';
-  let tempChar = null;
-  let completed = false;
-
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    for (let offset = 0; offset < 3; offset++) {
-      if (imageData.data[i + offset] % 2 === 0) {
-        tempBits += '0';
-      } else {
-        tempBits += '1';
-      }
-      if (tempBits.length === 8) {
-        tempChar = String.fromCharCode(parseInt(tempBits, 2));
-        if (tempChar === DELIMITER_CHARACTER) {
-          completed = true;
-          break;
-        }
-        ciphertext += tempChar;
-        tempBits = '';
-      }
-    }
-    if (completed) {
-      break;
-    }
+  const header = getMessageHeaderFromImageData(imageData);
+  if (header !== MESSAGE_HEADER) {
+    throw new Error('Message not found in the image');
   }
 
-  if (completed) {
-    try {
-      result = AES.decrypt(ciphertext, password).toString(Utf8);
-    } catch (error) {
-      console.error('=========== Decoding', error);
-    }
+  const range = getCiphertextRangeFromImageData(imageData);
+  if (!range) {
+    throw new Error('Message not found in the image');
   }
 
-  result = result || 'Message not found in the image.';
+  const ciphertext = getDecodedTextFromImageData(range, imageData);
+
+  const result = CryptoJS.AES.decrypt(ciphertext, password).toString(CryptoJS.enc.Utf8);
+  if (!result) {
+    throw new Error('Password is incorrect');
+  }
 
   return result;
 };
